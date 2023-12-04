136d11a1f05b75343612704ddc05bd228214f290
WouterLaenen <113888494+WouterLaenen@users.noreply.github.com>
2023-12-04 17:02:58 +0100

    Create main.py
    Pynq basis programma

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+import random
+
+def generate_random_number():
+    return random.randint(1, 100)  # Pas het bereik aan zoals nodig
+
+while True:
+    random_number = generate_random_number()
+    print(f"Willekeurig getal: {random_number}")
+    time.sleep(10)  # Wacht 10 seconden voordat het volgende getal wordt gegenereerd
